7df3222f83bd1142bf0beca330783b0550444d24
Kim Arum(kiki) <kimrumm@naver.com>
2024-10-16 22:32:10 +0900

    feat: 상품 단건 조회 구현

--- src/main/java/com/hhp/ecommerce/application/service/ProductService.java
+++ src/main/java/com/hhp/ecommerce/application/service/ProductService.java
@@ -1,21 +1,27 @@
 package com.hhp.ecommerce.application.service;
 
-import com.hhp.ecommerce.domain.model.Product;
-import com.hhp.ecommerce.infra.persistence.ProductRepository;
-import com.hhp.ecommerce.presentation.dto.response.ProductResponse;
-import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.hhp.ecommerce.domain.model.Product;
+import com.hhp.ecommerce.infra.persistence.ProductRepository;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class ProductService {
 
-    private final ProductRepository productRepository;
+	private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
-    }
+	public List<Product> getProducts() {
+		return productRepository.findAll();
+	}
+
+	public Product getProductById(Long productId) {
+		return productRepository.findById(productId)
+			.orElseThrow(() -> new NoSuchElementException("상품을 찾을 수 없습니다."));
+	}
 }
--- src/main/java/com/hhp/ecommerce/infra/persistence/ProductRepository.java
+++ src/main/java/com/hhp/ecommerce/infra/persistence/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.hhp.ecommerce.infra.persistence;
 
-import com.hhp.ecommerce.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.hhp.ecommerce.domain.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
