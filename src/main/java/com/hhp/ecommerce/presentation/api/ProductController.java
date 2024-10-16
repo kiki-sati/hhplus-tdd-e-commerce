36c9e0b6f260053fe398f22b7782d54a4aa48728
Kim Arum(kiki) <kimrumm@naver.com>
2024-10-16 22:31:02 +0900

    fix: Update 상품 목록 반환 타입 수정
    - .collect(Collectors.toList()) -> .toList() 로 수정하였다.
    - Java 16에서 도입된 toList() 메서드는 수정 불가능한 목록을 생성하여 예상치 못한 수정을 줄이고 코드 가독성을 개선하는 데 도움이 됩니다. Java 16 이상을 사용하는 경우 이 변경 사항은 불변성을 보장하면서 코드를 단순화하는 데 도움이 됩니다.

--- src/main/java/com/hhp/ecommerce/presentation/api/ProductController.java
+++ src/main/java/com/hhp/ecommerce/presentation/api/ProductController.java
@@ -3,40 +3,50 @@ package com.hhp.ecommerce.presentation.api;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-import com.hhp.ecommerce.application.service.ProductService;
-import com.hhp.ecommerce.presentation.dto.response.ProductResponse;
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hhp.ecommerce.application.service.ProductService;
+import com.hhp.ecommerce.domain.model.Product;
 import com.hhp.ecommerce.presentation.dto.ProductSalesResponse;
+import com.hhp.ecommerce.presentation.dto.response.ProductResponse;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/products")
 @AllArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    @GetMapping
-    public ResponseEntity<List<ProductResponse>> getProducts() {
-        List<ProductResponse> products = productService.getProducts().stream()
-                .map(product -> ProductResponse.fromProduct(product))
-                .collect(Collectors.toList());
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getProducts() {
+		List<ProductResponse> products = productService.getProducts().stream()
+			.map(ProductResponse::fromProduct)
+			.toList();
 
-        return ResponseEntity.ok(products);
-    }
+		return ResponseEntity.ok(products);
+	}
 
-    @GetMapping("/top")
-    public ResponseEntity<List<ProductSalesResponse>> getTopProducts() {
-        List<ProductSalesResponse> topProducts = new ArrayList<>();
-        topProducts.add(new ProductSalesResponse(UUID.randomUUID().toString(), "Laptop", 1500, 25));
-        topProducts.add(new ProductSalesResponse(UUID.randomUUID().toString(), "Smartphone", 800, 20));
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+		Product product = productService.getProductById(id);
+		ProductResponse productResponse = ProductResponse.fromProduct(product);
 
-        return ResponseEntity.ok(topProducts);
-    }
+		return ResponseEntity.ok(productResponse);
+	}
+
+	@GetMapping("/top")
+	public ResponseEntity<List<ProductSalesResponse>> getTopProducts() {
+		List<ProductSalesResponse> topProducts = new ArrayList<>();
+		topProducts.add(new ProductSalesResponse(UUID.randomUUID().toString(), "Laptop", 1500, 25));
+		topProducts.add(new ProductSalesResponse(UUID.randomUUID().toString(), "Smartphone", 800, 20));
+
+		return ResponseEntity.ok(topProducts);
+	}
 }
