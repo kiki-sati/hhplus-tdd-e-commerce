6eda7957867ae7a49eb0785ed86a68df3f9c39ae
kiki <kiki@plgrim.com>
2024-10-25 16:45:04 +0900

    fix: User 도메인 예약어 오류 수정

--- src/main/java/com/hhp/ecommerce/domain/model/User.java
+++ src/main/java/com/hhp/ecommerce/domain/model/User.java
@@ -2,11 +2,7 @@ package com.hhp.ecommerce.domain.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "\"user\"")
 public class User {
 
 	@Id
