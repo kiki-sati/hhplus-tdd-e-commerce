27f186fa644dc358493a2d7bc3cb86c355fa60c3
kiki <kiki@plgrim.com>
2024-11-01 16:06:29 +0900

    chore: 테스트용 DB 설정 추가

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name, email, balance, created_at, updated_at)
+VALUES (1, 'Alice', 'alice@example.com', 1000, NOW(), NOW());
+
+INSERT INTO products (id, name, price, stock, created_at, updated_at)
+VALUES (1, 'Laptop', 1500, 10, NOW(), NOW());
